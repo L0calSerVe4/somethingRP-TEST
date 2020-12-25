5e48d66ce82fa4531970f9ba58e55087c8d91de4
Nick Salt <46006335+BlueBerrySans365@users.noreply.github.com>
2020-12-26 00:05:34 +0200

    1

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using DiscordRPC;
+
+namespace something
+{
+    
+    public class RPC
+{
+    public DiscordRpcClient client;
+            public void Initialize(string state, string details, string LIT, string SIT) 
+            {
+                client = new DiscordRpcClient("619183056636477466");			
+                
+                client.Initialize();
+
+                client.SetPresence(new RichPresence()
+                {
+                    Details = details,
+                    State = state,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = "chill_zone",
+                        LargeImageText = LIT,
+                        SmallImageKey = "c_sharp",
+                        SmallImageText = SIT
+                    },
+                    Timestamps = Timestamps.Now
+                });
+            } 
+
+}
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            RPC b = new RPC();
+            // string SITT = null;
+            // string LITT = null;
+            // string statet = null;
+            // string detailst = null;
+            Console.Clear();
+            Console.Title = "Something RP Test";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Something RP Test || .Net Framework v4.8");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("'I don't know how i made this' - NickSaltFoxu");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            bool[] argTaken = new bool[2] {false, false};
+            for (int i = 0; i < args.Length; i++)
+            {
+                string _arg = args[i].ToLower();
+                if (args.Length > i)
+                {
+                    if (_arg.Equals("--help") || _arg.Equals("-h"))
+                    {
+                        Console.WriteLine("test");
+                        return;
+                    }
+                    else if (_arg.Equals("--test"))
+                    {
+                        b.Initialize("C# Example", "Running from CMD", "Something RP Test", ".Net Framework v4.8");
+                        Console.WriteLine("Ready!");
+                        Console.WriteLine("To stop app press CTRL+C");
+                        while(true){
+                            System.Threading.Thread.Sleep(5000);
+                        }
+                    }
+                }
+            }
+            if (argTaken[0]){}
+            else
+            {
+                Console.WriteLine("\nUsage:");
+                Console.WriteLine("something.exe [-st small image text*] [-lt large image text*] [-s state text*] [-d details text*]\r\n" +
+                                  "\t*required");
+                return;
+            }
+        }
+        
+        
+    }
+}
